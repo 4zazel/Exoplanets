208b8d4a98c66cf0e7d2f0e4ccd6e20a873f31b3
4zazel <109144257+4zazel@users.noreply.github.com>
2024-10-05 12:26:32 +0200

    almost

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function PlanetCard()
+{
+    return(
+        <div class="card m-3">
+        <img src="" class="card-img-top" alt="..."/>
+        <div class="card-body">
+            <h5 class="card-title">Planeta1</h5>
+            <a href="..." class="btn btn-primary">Test</a>
+        </div>
+    </div>
+    )
+}
+
+export default PlanetCard;
--- src/components/PlanetsList.jsx
+++ src/components/PlanetsList.jsx
@@ -1,23 +1,42 @@
 import React from "react";
+import PlanetCard from "./PlanetCard";
 
 function PlanetsList()
 {
     return(
         <div clas="container text-center">
             <div class="row">
-                <div class="col">Col11</div>
-                <div class="col">Col12</div>
-                <div class="col">Col13</div>
+                <div class="col">
+                    <PlanetCard/>
+                </div>
+                <div class="col">
+                    <PlanetCard/>
+                </div>
+                <div class="col">
+                    <PlanetCard/>
+                </div>
             </div>
             <div class="row">
-                <div class="col">Col21</div>
-                <div class="col">Col22</div>
-                <div class="col">Col23</div>
+                <div class="col">
+                    <PlanetCard/>    
+                </div>
+                <div class="col">
+                    <PlanetCard/>
+                </div>
+                <div class="col">
+                    <PlanetCard/>
+                </div>
             </div>
             <div class="row">
-                <div class="col">Col21</div>
-                <div class="col">Col22</div>
-                <div class="col">Col23</div>
+                <div class="col">
+                    <PlanetCard/>
+                </div>
+                <div class="col">
+                    <PlanetCard/>
+                </div>
+                <div class="col">
+                    <PlanetCard/>
+                </div>
             </div>
         </div>
     )
