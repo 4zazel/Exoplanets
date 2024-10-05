a3cca9f9737aaf5159bbfe08810ff25f142e70b9
4zazel <109144257+4zazel@users.noreply.github.com>
2024-10-05 16:26:56 +0200

    update planet desc

--- src/components/PlanetDescription.jsx
+++ src/components/PlanetDescription.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 
-function PlanetDescription()
+function PlanetDescription(id, name, description, shortDescription, dateOfDiscovery, mass, type, size)
 {
-
+    return(
+        <>
+            <h1 className="text-center">Planet 1 - The first exoplanet ever discovered!</h1>
+            <div className="m-5 row">
+                <div className="col">
+                    <h4 className="text-left">This is placeholder text.This is placeholder text.This is placeholder text.This is placeholder text.This is placeholder text.This is placeholder text.</h4>
+                    <br/><br/><br/><br/>
+                    <h3>Gravity: 10</h3>
+                    <h3>Type: Gas</h3>
+                    <h3>Size: 4km</h3>
+                    <h3>Date of Discovery: 5.05.1999</h3>
+                </div>
+                <div className="col">
+                    <img src="src/assets/placeholder.JPG" href="#"></img>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default PlanetDescription;
